68ee7071bda9f28d7a56e285e136960294b24f0f
KARTHIKEYAN S <122821663+ID-KARTHIKEYAN@users.noreply.github.com>
2026-06-23 09:16:51 +0530

    sync: WEATHER_L0.sql [v1.0] [untagged] deployed: 2026-06-23 03:46 | branch: main | layer: L0 [Jenkins]

--- META_STORE_SQL_SCRIPTS/WEATHER_L0.sql
+++ META_STORE_SQL_SCRIPTS/WEATHER_L0.sql
@@ -1,45 +1,17 @@
 -- ==============================================
 -- Workspace : karthik_workspace
 -- Group     : WEATHER_L0
--- Generated : 2026-06-18 05:17:04
+-- Bundle    : [v1.0] [untagged] deployed: 2026-06-23 03:46 | branch: main | layer: L0
 -- ==============================================
 
 MERGE INTO demo_catalog.admin.data_flow_control_header AS tgt
-USING (
-  SELECT
-    'WEATHER_L0'           AS DATA_FLOW_GROUP_ID,
-    'WEATHER_L0_JOB'       AS JOB_NAME,
-    '/Workspace/Users/svkarthick0@gmail.com/DATA_INTEGRATION/notebooks/run_framework'      AS NOTEBOOK_PATH,
-    'Y'                    AS IS_ACTIVE,
-    current_timestamp()    AS CREATED_DATE,
-    current_timestamp()    AS UPDATED_DATE
-) AS src
+USING (SELECT 'WEATHER_L0' AS DATA_FLOW_GROUP_ID, 'WEATHER_L0_JOB' AS JOB_NAME, '/Workspace/Users/svkarthick0@gmail.com/DATA_INTEGRATION/notebooks/run_framework' AS NOTEBOOK_PATH, 'Y' AS IS_ACTIVE, current_timestamp() AS CREATED_DATE, current_timestamp() AS UPDATED_DATE) AS src
 ON tgt.DATA_FLOW_GROUP_ID = src.DATA_FLOW_GROUP_ID
-WHEN MATCHED THEN UPDATE SET
-    tgt.NOTEBOOK_PATH = src.NOTEBOOK_PATH,
-    tgt.IS_ACTIVE     = src.IS_ACTIVE,
-    tgt.UPDATED_DATE  = src.UPDATED_DATE
+WHEN MATCHED THEN UPDATE SET tgt.NOTEBOOK_PATH = src.NOTEBOOK_PATH, tgt.IS_ACTIVE = src.IS_ACTIVE, tgt.UPDATED_DATE = src.UPDATED_DATE
 WHEN NOT MATCHED THEN INSERT *;
 
 MERGE INTO demo_catalog.admin.data_flow_l0_detail AS tgt
-USING (
-  SELECT
-    'WEATHER_L0'  AS DATA_FLOW_GROUP_ID,
-    'https://raw.githubusercontent.com/Devasree03/ETL_JEN/refs/heads/main/data/weather.csv'     AS SOURCE_URL,
-    'bronze'     AS TARGET_SCHEMA,
-    'Wea_data'     AS TARGET_TABLE,
-    'csv'     AS FILE_FORMAT,
-    'FULL'     AS LOAD_TYPE,
-    'Y'           AS IS_ACTIVE,
-    current_timestamp() AS CREATED_DATE,
-    current_timestamp() AS UPDATED_DATE
-) AS src
-ON  tgt.DATA_FLOW_GROUP_ID = src.DATA_FLOW_GROUP_ID
-AND tgt.TARGET_TABLE       = src.TARGET_TABLE
-WHEN MATCHED THEN UPDATE SET
-    tgt.SOURCE_URL   = src.SOURCE_URL,
-    tgt.FILE_FORMAT  = src.FILE_FORMAT,
-    tgt.LOAD_TYPE    = src.LOAD_TYPE,
-    tgt.IS_ACTIVE    = src.IS_ACTIVE,
-    tgt.UPDATED_DATE = src.UPDATED_DATE
+USING (SELECT 'WEATHER_L0' AS DATA_FLOW_GROUP_ID, 'https://raw.githubusercontent.com/Devasree03/ETL_JEN/refs/heads/main/data/weather.csv' AS SOURCE_URL, 'bronze' AS TARGET_SCHEMA, 'Wea_data' AS TARGET_TABLE, 'csv' AS FILE_FORMAT, 'FULL' AS LOAD_TYPE, 'Y' AS IS_ACTIVE, current_timestamp() AS CREATED_DATE, current_timestamp() AS UPDATED_DATE) AS src
+ON tgt.DATA_FLOW_GROUP_ID = src.DATA_FLOW_GROUP_ID AND tgt.TARGET_TABLE = src.TARGET_TABLE
+WHEN MATCHED THEN UPDATE SET tgt.SOURCE_URL = src.SOURCE_URL, tgt.FILE_FORMAT = src.FILE_FORMAT, tgt.LOAD_TYPE = src.LOAD_TYPE, tgt.IS_ACTIVE = src.IS_ACTIVE, tgt.UPDATED_DATE = src.UPDATED_DATE
 WHEN NOT MATCHED THEN INSERT *;
