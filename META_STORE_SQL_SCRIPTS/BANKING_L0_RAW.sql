ca8aba2b4d1624622a302a1c37987088802ec886
KARTHIKEYAN S <122821663+ID-KARTHIKEYAN@users.noreply.github.com>
2026-06-23 09:18:20 +0530

    sync: BANKING_L0_RAW.sql [v1.0] [untagged] deployed: 2026-06-23 03:48 | branch: main | layer: ALL [Jenkins]

---
+++ META_STORE_SQL_SCRIPTS/BANKING_L0_RAW.sql
@@ -0,0 +1,17 @@
+-- ==============================================
+-- Workspace : karthik_workspace
+-- Group     : BANKING_L0_RAW
+-- Bundle    : [v1.0] [untagged] deployed: 2026-06-23 03:48 | branch: main | layer: ALL
+-- ==============================================
+
+MERGE INTO demo_catalog.admin.data_flow_control_header AS tgt
+USING (SELECT 'BANKING_L0_RAW' AS DATA_FLOW_GROUP_ID, 'BANKING_L0_RAW_JOB' AS JOB_NAME, '/Workspace/Users/svkarthick0@gmail.com/run_framework' AS NOTEBOOK_PATH, 'Y' AS IS_ACTIVE, current_timestamp() AS CREATED_DATE, current_timestamp() AS UPDATED_DATE) AS src
+ON tgt.DATA_FLOW_GROUP_ID = src.DATA_FLOW_GROUP_ID
+WHEN MATCHED THEN UPDATE SET tgt.NOTEBOOK_PATH = src.NOTEBOOK_PATH, tgt.IS_ACTIVE = src.IS_ACTIVE, tgt.UPDATED_DATE = src.UPDATED_DATE
+WHEN NOT MATCHED THEN INSERT *;
+
+MERGE INTO demo_catalog.admin.data_flow_l0_detail AS tgt
+USING (SELECT 'BANKING_L0_RAW' AS DATA_FLOW_GROUP_ID, 'https://raw.githubusercontent.com/swapniltake1/data-engineering-practice-datasets/refs/heads/main/CSV/banking.csv' AS SOURCE_URL, 'bronze' AS TARGET_SCHEMA, 'bank_data' AS TARGET_TABLE, 'csv' AS FILE_FORMAT, 'FULL' AS LOAD_TYPE, 'Y' AS IS_ACTIVE, current_timestamp() AS CREATED_DATE, current_timestamp() AS UPDATED_DATE) AS src
+ON tgt.DATA_FLOW_GROUP_ID = src.DATA_FLOW_GROUP_ID AND tgt.TARGET_TABLE = src.TARGET_TABLE
+WHEN MATCHED THEN UPDATE SET tgt.SOURCE_URL = src.SOURCE_URL, tgt.FILE_FORMAT = src.FILE_FORMAT, tgt.LOAD_TYPE = src.LOAD_TYPE, tgt.IS_ACTIVE = src.IS_ACTIVE, tgt.UPDATED_DATE = src.UPDATED_DATE
+WHEN NOT MATCHED THEN INSERT *;
