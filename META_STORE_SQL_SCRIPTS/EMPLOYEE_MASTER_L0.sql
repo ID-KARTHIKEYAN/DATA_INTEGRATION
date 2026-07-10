4d086286b26a1a08a8f4624ba6f48eaf3a723248
KARTHIKEYAN S <122821663+ID-KARTHIKEYAN@users.noreply.github.com>
2026-07-10 11:45:44 +0530

    sync: EMPLOYEE_MASTER_L0.sql [v1.0][untagged] 2026-07-10 06:15 | L0 | main [Jenkins]

--- META_STORE_SQL_SCRIPTS/EMPLOYEE_MASTER_L0.sql
+++ META_STORE_SQL_SCRIPTS/EMPLOYEE_MASTER_L0.sql
@@ -1,4 +1,4 @@
--- karthik_workspace | EMPLOYEE_MASTER_L0 | [v1.0][untagged] 2026-06-26 06:03 | L0 | main
+-- karthik_workspace | EMPLOYEE_MASTER_L0 | [v1.0][untagged] 2026-07-10 06:15 | L0 | main
 
 MERGE INTO demo_catalog.admin.data_flow_control_header AS tgt
 USING (SELECT 'EMPLOYEE_MASTER_L0' AS DATA_FLOW_GROUP_ID,'EMPLOYEE_MASTER_L0_JOB' AS JOB_NAME,'/Workspace/Repos/svkarthick0@gmail.com/DATA_INTEGRATION/notebooks/run_framework' AS NOTEBOOK_PATH,'Y' AS IS_ACTIVE,current_timestamp() AS CREATED_DATE,current_timestamp() AS UPDATED_DATE) AS src
