e75038dab5edea2b221fcb6b3de5174c5ff358a2
KARTHIKEYAN S <122821663+ID-KARTHIKEYAN@users.noreply.github.com>
2026-07-10 11:44:41 +0530

    sync: FINANCE_MEDALLION_L0.sql [v1.0][untagged] 2026-07-10 06:14 | L0 | main [Jenkins]

--- META_STORE_SQL_SCRIPTS/FINANCE_MEDALLION_L0.sql
+++ META_STORE_SQL_SCRIPTS/FINANCE_MEDALLION_L0.sql
@@ -1,7 +1,7 @@
--- karthik_workspace | FINANCE_MEDALLION_L0 | [v1.0][untagged] 2026-07-10 06:12 | L0 | main
+-- MARAN_workspace | FINANCE_MEDALLION_L0 | [v1.0][untagged] 2026-07-10 06:14 | L0 | main
 
 MERGE INTO demo_catalog.admin.data_flow_control_header AS tgt
-USING (SELECT 'FINANCE_MEDALLION_L0' AS DATA_FLOW_GROUP_ID,'FINANCE_MEDALLION_L0_JOB' AS JOB_NAME,'/Workspace/Repos/svkarthick0@gmail.com/DATA_INTEGRATION/notebooks/run_framework' AS NOTEBOOK_PATH,'Y' AS IS_ACTIVE,current_timestamp() AS CREATED_DATE,current_timestamp() AS UPDATED_DATE) AS src
+USING (SELECT 'FINANCE_MEDALLION_L0' AS DATA_FLOW_GROUP_ID,'FINANCE_MEDALLION_L0_JOB' AS JOB_NAME,'/Workspace/Repos/ma34884730@gmail.com/DATA_INTEGRATION/notebooks/run_framework' AS NOTEBOOK_PATH,'Y' AS IS_ACTIVE,current_timestamp() AS CREATED_DATE,current_timestamp() AS UPDATED_DATE) AS src
 ON tgt.DATA_FLOW_GROUP_ID=src.DATA_FLOW_GROUP_ID
 WHEN MATCHED THEN UPDATE SET tgt.NOTEBOOK_PATH=src.NOTEBOOK_PATH,tgt.IS_ACTIVE=src.IS_ACTIVE,tgt.UPDATED_DATE=src.UPDATED_DATE
 WHEN NOT MATCHED THEN INSERT *;
