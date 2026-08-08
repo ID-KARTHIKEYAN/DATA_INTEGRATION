87e6eadf7de4fafc56408ab0629f8578f8aacaa8
KARTHIKEYAN S <122821663+ID-KARTHIKEYAN@users.noreply.github.com>
2026-08-08 22:39:46 +0530

    sync: WEATHER_L0.sql [v1.0][untagged] 2026-08-08 17:09 | L0 | main [Jenkins]

--- META_STORE_SQL_SCRIPTS/WEATHER_L0.sql
+++ META_STORE_SQL_SCRIPTS/WEATHER_L0.sql
@@ -1,4 +1,4 @@
--- karthik_workspace | WEATHER_L0 | [v1.0][untagged] 2026-06-26 06:15 | L0 | main
+-- karthik_workspace | WEATHER_L0 | [v1.0][untagged] 2026-08-08 17:09 | L0 | main
 
 MERGE INTO demo_catalog.admin.data_flow_control_header AS tgt
 USING (SELECT 'WEATHER_L0' AS DATA_FLOW_GROUP_ID,'WEATHER_L0_JOB' AS JOB_NAME,'/Workspace/Users/svkarthick0@gmail.com/DATA_INTEGRATION/notebooks/run_framework' AS NOTEBOOK_PATH,'Y' AS IS_ACTIVE,current_timestamp() AS CREATED_DATE,current_timestamp() AS UPDATED_DATE) AS src
